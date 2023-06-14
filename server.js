55dfb3443d3da2b2e835a0e1f14fd347bc37a056
Josh Sandquist <joshdsand@gmail.com>
2023-06-14 13:52:05 -0700

    updated server file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`API server now running on port ${PORT}!`);
+});

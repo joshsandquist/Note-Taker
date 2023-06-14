5f852e0110d37368e80b85f2774146364c34ae83
Josh Sandquist <joshdsand@gmail.com>
2023-06-14 14:12:49 -0700

    added routes

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-
 app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now running on port ${PORT}!`);

6bd01368443972dad32b2bf5c2026b824f9a4ff7
Josh Sandquist <joshdsand@gmail.com>
2023-06-14 15:30:42 -0700

    fixed error of route usage in server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
+//setting up express server
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+//importing routes
 const apiRoutes = require('./routes/apiRoutes.js')
 const htmlRoutes = require('./routes/htmlRoutes.js')
-
+//boilerplate middleware
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.static('public'));
 
-app.use('/', htmlRoutes);
 app.use('/api', apiRoutes)
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now running on port ${PORT}!`);
